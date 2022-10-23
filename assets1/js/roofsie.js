0f8d7299cf9786e7b3fe6857eb7d7c4bf1ca4d16
buckapi <buckapis@gmail.com>
2022-10-23 04:50:14 -0400

    seguras

--- assets1/js/roofsie.js
+++ assets1/js/roofsie.js
@@ -930,3 +930,18 @@
     });
   }
 })(jQuery);
+
+// -------------------
+function growDiv() {
+    var growDiv = document.getElementById('grow');
+    if (growDiv.clientHeight) {
+      growDiv.style.height = 0;
+    } else {
+      var wrapper = document.querySelector('.measuringWrapper');
+      growDiv.style.height = wrapper.clientHeight + "px";
+    }
+document.getElementById("more-button").value=document.getElementById("more-button").value=='Mas'?'Menos':'Mas';
+}
+
+// -----------------------
+
